8ddec60839f647eeafa0a8ac8eeb5c033084a55f
NugiSquare <nugisquare@gmail.com>
2021-03-15 00:23:45 +0900

    complete assignment1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
-import org.w3c.dom.Document;
+import org.jsoup.nodes.Document;
 import org.w3c.dom.Element;
+
+import org.jsoup.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -10,38 +12,48 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws ParserConfigurationException, TransformerException, FileNotFoundException {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException {
+
+        String[] documentlist =new String[]{"떡","라면","아이스크림","초밥","파스타"};
 
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-        Document doc = docBuilder.newDocument();
+        org.w3c.dom.Document doc = docBuilder.newDocument();
 
-        // book element
-        Element movie = doc.createElement("movie");
-        doc.appendChild(movie);
+        // docs element
+        Element docs = doc.createElement("docs");
+        doc.appendChild(docs);
 
-        // code element
-        Element code = doc.createElement("code");
-        movie.appendChild(code);
+        for (int i = 0; i < documentlist .length; i++) {
 
-        // attribute value type = SF
-        code.setAttribute("type", "SF");
+            File input = new File("src/data/"+documentlist[i]+".html");
+            Document document = Jsoup.parse(input, "UTF-8");
 
-        // name element
-        Element movieName = doc.createElement("movieName");
-        movieName.appendChild(doc.createTextNode("테넷"));
-        code.appendChild(movieName);
+            // DocID element
+            Element did = doc.createElement("doc");
+            docs.appendChild(did);
 
-        //movieDirector element
-        Element movieDirector = doc.createElement("movieDirector");
-        movieDirector.appendChild(doc.createTextNode("크리스토퍼 놀란"));
-        code.appendChild(movieDirector);
+            // attribute value type = id
+            did.setAttribute("id", Integer.toString(i+1));
+
+            // title element
+            Element doctitle = doc.createElement("title");
+            doctitle.appendChild(doc.createTextNode(document.title()));
+            did.appendChild(doctitle);
+
+            //movieDirector element
+            Element docbody = doc.createElement("body");
+            docbody.appendChild(doc.createTextNode(document.select("p").text()));
+            did.appendChild(docbody);
+
+
+        }
 
         // Writing XML
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
@@ -50,7 +62,7 @@ public class Main {
         transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 
         DOMSource source = new DOMSource(doc);
-        StreamResult result = new StreamResult(new FileOutputStream(new File("src/data/book.xml")));
+        StreamResult result = new StreamResult(new FileOutputStream(new File("src/data/result.xml")));
 
         transformer.transform(source, result);
 
