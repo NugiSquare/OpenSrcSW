332af2c8c8bec503fef55c8ee29ea47748dbfc3a
NugiSquare <nugisquare@gmail.com>
2021-03-25 23:11:28 +0900

    add gitignore and some noise

--- src/com/company/indexer.java
+++ src/com/company/indexer.java
@@ -1,12 +1,29 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+
 public class indexer {
 
     public static void main(String[] args) {
 
     }
 
-    public void indexing(String arg) {
+    @SuppressWarnings({"rawtypes", "unchecked", "nls"})
+    public void indexing(String arg) throws IOException {
+
+        FileOutputStream fileStream = new FileOutputStream("src/data/movie.ser");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileStream);
+
+        HashMap MovieMap = new HashMap();
+
+        objectOutputStream.writeObject(MovieMap);
+        objectOutputStream.close();
+
+
 
     }
 }
